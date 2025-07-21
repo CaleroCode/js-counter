d1c425d22b23e7db17fd3c52186f8e828f15a37c
Iván Calero <ivanicaleroo@gmail.com>
2025-07-21 18:52:37 +0200

    Completed

--- src/js/counter.js
+++ src/js/counter.js
@@ -1,13 +1,32 @@
 function add(){
     //Escribir tu código que aumenta
+    const aumentarValor = document.querySelector("#number");
+    let valorActual = parseInt(aumentarValor.textContent); 
+    valorActual++
+    aumentarValor.textContent = valorActual;
 }
+    const botonAumento = document.querySelector("#add");
+    botonAumento.addEventListener("click", add);
 
 function reset(){
     //Escribir tu código que hace un reset a 0
+    const valorReset = document.querySelector("#number");
+    valorReset.textContent = 0;
 }
+    const botonReset = document.querySelector("#reset");
+    botonReset.addEventListener("click", reset);
 
 function decrease(){
     //Escribir tu código que resta
+    const restarValor = document.querySelector("#number");
+    let valorActual = parseInt(restarValor.textContent);
+
+        if (valorActual > 0) {
+            valorActual--;
+            restarValor.textContent = valorActual;
+        }
 };
 
+    const botonDisminuir = document.querySelector("#decrease");
+    botonDisminuir.addEventListener("click", decrease);
 export {add, reset, decrease}
--- src/js/events.js
+++ src/js/events.js
@@ -2,3 +2,11 @@ import {add, reset, decrease} from './counter.js'
 
 
 //Aquí estarán los eventos que ejecutarán cada una de las funciones realizadas
+
+const botonAumento = document.getElementById("add");
+const botonDisminuir = document.getElementById("decrease");
+const botonReset = document.getElementById("reset");
+
+    botonAumento.addEventListener("click", add);
+    botonDisminuir.addEventListener("click", decrease)
+    botonReset.addEventListener("click", reset);
